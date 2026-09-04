0192c8fb63329f9f014b0ca676d96be29f10f2ca
Henrico <henrico.ferreira@aluno.senai.br>
2026-09-04 16:21:53 -0300

    feat: quase finaliza o fullstack

--- src/database/connection.js
+++ src/database/connection.js
@@ -9,7 +9,7 @@ const { Pool } = pkg;
 // Configuração do pool de conexões com o PostgreSQL
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
+  port: Number(process.env.DB_PORT) || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
